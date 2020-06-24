49a81433c742d745814c593a1221262d1fdbbf8c
Fernando Carniel <fernando.carniel@arquivei.com.br>
2020-06-24 14:19:50 -0300

    Adiciona System Tray para exibição de notificações
    Adiciona o suporte ao SystemTray para exibição de notificações
    de erros, download ou upload concluido com sucesso.
    
    Adiciona também a possibilidade de minimizar o aplicativo para a task bar.
    
    Adiciona ícone do Arquivei para a exibição no aplicativo e na task bar.
    
    Foi adicionada uma lógica para o controle de exibição de notificações para
    exibir apenas uma única notificação por vez para cada status, pois hj fazemos
    um polling no backend server a cada 5s para atualizar a interface gráfica e seria
    inviável exibir uma notificação a cada 5s para o usuário.
    
    Links:
    + https://arquivei.atlassian.net/browse/BONDERS-563

---
+++ systemtray.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"arquivei.com.br/daemon-ui/client/commands/clientstatus"
+	"github.com/therecipe/qt/widgets"
+)
+
+const systemTrayTitle = "Sincroniza Notas"
+
+const (
+	statusProcessing      = "STATUS_PROCESSING"
+	statusFinished        = "STATUS_FINISHED"
+	statusErrorConnection = "STATUS_ERROR_CONNECTION"
+)
+
+//QSystemTrayIconCustomSlot ...
+type QSystemTrayIconCustomSlot struct {
+	widgets.QSystemTrayIcon
+	InternetConnectionTrayViewed bool
+	DownloadCompletedTrayViewed  bool
+	UploadCompletedTrayViewed    bool
+
+	_ func(f func()) `slot:"triggerSlot,auto"`
+}
+
+//triggerSlot just needs to call the passed function to execute it inside the main thread
+func (tray *QSystemTrayIconCustomSlot) triggerSlot(f func()) { f() }
+
+func (tray *QSystemTrayIconCustomSlot) showDownloadStatus(clientStatus clientstatus.Response) {
+	var message string
+	var icon widgets.QSystemTrayIcon__MessageIcon
+
+	switch clientStatus.Download.ProcessingStatus {
+	case statusProcessing:
+		tray.InternetConnectionTrayViewed = false
+		tray.DownloadCompletedTrayViewed = false
+		return
+	case statusFinished:
+		if tray.DownloadCompletedTrayViewed {
+			return
+		}
+		message = "O download dos documentos terminou"
+		icon = widgets.QSystemTrayIcon__Information
+		tray.DownloadCompletedTrayViewed = true
+	case statusErrorConnection:
+		if tray.InternetConnectionTrayViewed {
+			return
+		}
+		message = "Sem conexão com a internet"
+		icon = widgets.QSystemTrayIcon__Critical
+		tray.InternetConnectionTrayViewed = true
+	default:
+		return
+	}
+
+	tray.TriggerSlot(func() {
+		tray.ShowMessage(systemTrayTitle, message, icon, 5000)
+	})
+}
+
+func (tray *QSystemTrayIconCustomSlot) showUploadStatus(clientStatus clientstatus.Response) {
+	var message string
+	var icon widgets.QSystemTrayIcon__MessageIcon
+
+	switch clientStatus.Upload.ProcessingStatus {
+	case statusProcessing:
+		tray.InternetConnectionTrayViewed = false
+		tray.UploadCompletedTrayViewed = false
+		return
+	case statusFinished:
+		if tray.UploadCompletedTrayViewed {
+			return
+		}
+		message = "O envio dos documentos terminou"
+		icon = widgets.QSystemTrayIcon__Information
+		tray.UploadCompletedTrayViewed = true
+	case statusErrorConnection:
+		if tray.InternetConnectionTrayViewed {
+			return
+		}
+		message = "Sem conexão com a internet"
+		icon = widgets.QSystemTrayIcon__Critical
+		tray.InternetConnectionTrayViewed = true
+	default:
+		return
+	}
+
+	tray.TriggerSlot(func() {
+		tray.ShowMessage(systemTrayTitle, message, icon, 5000)
+	})
+}
